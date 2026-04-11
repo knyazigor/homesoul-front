b1fb0f43d0883f360c746c1e9544094536d61106
Igor Shaymukhametov <shaym@yandex.ru>
2026-04-11 17:57:12 +0500

    refactor: next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    domains: ["api.dushavashegodoma.ru", "localhost", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dushavashegodoma.ru",
+        port: "",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+
     formats: ["image/webp", "image/avif"],
   },
 };
