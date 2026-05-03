3d1d3f4065a426617c6ff9647be48d48bd831f6b
Igor Shaymukhametov <shaym@yandex.ru>
2026-05-03 22:19:36 +0500

    debug docs

--- src/actions/documents.ts
+++ src/actions/documents.ts
@@ -1,44 +1,22 @@
 "use server";
 
 import { strapiClient } from "@/lib/strapi";
-import { unstable_cache } from "next/cache";
 import { Document } from "@/lib/types/document";
 
 /**
  * Получение всех документов
  */
-export const getAllDocuments = unstable_cache(
-  async (): Promise<Document[]> => {
-    const documents = await strapiClient.getAllDocuments();
+export const getAllDocuments = async (): Promise<Document[]> => {
+  const documents = await strapiClient.getAllDocuments();
 
-    return documents;
-  },
-  ["all-documents"],
-  {
-    revalidate: 3600,
-    tags: ["documents"],
-  },
-);
+  return documents;
+};
+
 /**
  * Получение документа по ID
  */
-export const getDocument = unstable_cache(
-  async (id: string | number): Promise<Document | null> => {
-    return await strapiClient.getDocument(id);
-  },
-  ["document"],
-  {
-    revalidate: 3600,
-    tags: ["documents"],
-  },
-);
-
-/**
- * Принудительный сброс кэша
- */
-export const revalidateDocumentsCache = async () => {
-  "use server";
-
-  const { revalidateTag } = await import("next/cache");
-  revalidateTag("documents", "page");
+export const getDocument = async (
+  id: string | number,
+): Promise<Document | null> => {
+  return await strapiClient.getDocument(id);
 };
